2222d8bad5796153e5e90620fe6d4df1a3f31016
anadabrowska <anadabrowska98@gmail.com>
2018-11-11 15:13:05 +0100

    pull request fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin123@example.com', password: 'admin2', password_confirmation: 'admin2') if Rails.env.development?
