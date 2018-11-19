b76e294b9782319fb3b9428d1bd4d64221690763
adrianblonski <adblonski1@gmail.com>
2018-11-19 20:23:33 +0100

    Logic extracted to models. RSpec added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Product < ApplicationRecord
   validates :name, length: { in: 2..100 }
   validates :shop, length: { in: 2..100 }
@@ -7,12 +5,28 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :quantity, presence: true, numericality: { other_than: 0 }
 
+  def values
+    vals = []
+    attributes.values[1..-3].each do |x|
+      vals.push((x.is_a?(Float) ? format('%.2f', x) : x))
+    end
+    vals
+  end
+
+  def self.categories_names
+    names = []
+    all.column_names[1..-3].each do |name|
+      names.push(name.capitalize.tr('_', ' '))
+    end
+    names
+  end
+
   def self.total_price
     total = 0
     all.each { |product| total += product.price * product.quantity }
     total
   end
-  
+
   def self.total_price_by_category
     total = {}
     all.each do |product|
